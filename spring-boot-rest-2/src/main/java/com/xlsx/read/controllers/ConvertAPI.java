5343c7988d8c1d72ed9d2f15001b26ce6266a74f
ashu <recharge.ashu@gmail.com>
2022-08-03 01:00:29 +0530

    Updated path to application properties

--- spring-boot-rest-2/src/main/java/com/xlsx/read/controllers/ConvertAPI.java
+++ spring-boot-rest-2/src/main/java/com/xlsx/read/controllers/ConvertAPI.java
@@ -7,6 +7,7 @@ import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -24,13 +25,15 @@ import com.google.gson.JsonObject;
 @Controller
 public class ConvertAPI {
 	
+	@Value("${app.pathtoexcel}")
+	private String pathtoexcel;
 	
 	@RequestMapping(value = "/parseExcel", method = RequestMethod.POST,consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
 	@ResponseBody
 	String auth(@RequestParam Map<String,String> allParams) {
 		System.out.println("Parameters are " + allParams.entrySet());
 		allParams.forEach((k,v) -> System.out.println("Key = " + k + ", Value = " + v));
-		String FILE_NAME = "C:\\Users\\Ashutosh\\git\\spring-boot-rest-2\\src\\main\\resources\\excelread.xlsx";
+		String FILE_NAME = pathtoexcel;
 		FileInputStream excelFile;
 		try {
 			excelFile = new FileInputStream(FILE_NAME);
